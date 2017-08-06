9c20f7da7849703a8693fec055eddfcaad6ee7c8
Mike Eason <mike.eason@mail.com>
2017-08-06 17:08:03 +0100

    Trying to fix routing round 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,35 +22,35 @@
                     <h3>Documentation</h3>
                     <ul class="uk-nav uk-nav-default">
                         <li class="uk-nav-header">StockWise</li>
-                        <li><a href="/stockwise">Concept</a></li>
-                        <li><a href="/stockwise#searchingandscanning">Searching and Scanning</a></li>
-                        <li><a href="/stockwise#locations">Locations</a></li>
-                        <li><a href="/stockwise#stockenquiry">Stock Enquiry</a></li>
-                        <li><a href="/stockwise#suppliers">Suppliers</a></li>
-                        <li><a href="/stockwise#stockitemhistory">Stock Item History</a></li>
-                        <li><a href="/stockwise#stockin">Stock In</a></li>
-                        <li><a href="/stockwise#stockcount">Stock Count</a></li>
-                        <li><a href="/stockwise#purchaseorders">Purchase Orders</a></li>
-                        <li><a href="/stockwise#permissions">Admin Mode and Permissions</a></li>
-                        <li><a href="/stockwise#settings">Settings</a></li>
+                        <li><a href="/rigi-docs/stockwise">Concept</a></li>
+                        <li><a href="/rigi-docs/stockwise#searchingandscanning">Searching and Scanning</a></li>
+                        <li><a href="/rigi-docs/stockwise#locations">Locations</a></li>
+                        <li><a href="/rigi-docs/stockwise#stockenquiry">Stock Enquiry</a></li>
+                        <li><a href="/rigi-docs/stockwise#suppliers">Suppliers</a></li>
+                        <li><a href="/rigi-docs/stockwise#stockitemhistory">Stock Item History</a></li>
+                        <li><a href="/rigi-docs/stockwise#stockin">Stock In</a></li>
+                        <li><a href="/rigi-docs/stockwise#stockcount">Stock Count</a></li>
+                        <li><a href="/rigi-docs/stockwise#purchaseorders">Purchase Orders</a></li>
+                        <li><a href="/rigi-docs/stockwise#permissions">Admin Mode and Permissions</a></li>
+                        <li><a href="/rigi-docs/stockwise#settings">Settings</a></li>
                     </ul>
                     <ul class="uk-nav uk-nav-default">
                         <li class="uk-nav-header">ePickList</li>
-                        <li><a href="/epicklist">Concept</a></li>
-                        <li><a href="/epicklist#singleorderpick">Single Order Pick</a></li>
-                        <li><a href="/epicklist#bulkorderpick">Bulk Order Pick</a></li>
-                        <li><a href="/epicklist#pickamount">Pick Amount</a></li>
-                        <li><a href="/epicklist#picksource">Pick Source</a></li>
-                        <li><a href="/epicklist#pickoptions">Pick Options</a></li>
-                        <li><a href="/epicklist#pickrequirements">Pick Requirements</a></li>
-                        <li><a href="/epicklist#pickactions">Pick Actions</a></li>
-                        <li><a href="/epicklist#settings">Additional Settings</a></li>
+                        <li><a href="/rigi-docs/epicklist">Concept</a></li>
+                        <li><a href="/rigi-docs/epicklist#singleorderpick">Single Order Pick</a></li>
+                        <li><a href="/rigi-docs/epicklist#bulkorderpick">Bulk Order Pick</a></li>
+                        <li><a href="/rigi-docs/epicklist#pickamount">Pick Amount</a></li>
+                        <li><a href="/rigi-docs/epicklist#picksource">Pick Source</a></li>
+                        <li><a href="/rigi-docs/epicklist#pickoptions">Pick Options</a></li>
+                        <li><a href="/rigi-docs/epicklist#pickrequirements">Pick Requirements</a></li>
+                        <li><a href="/rigi-docs/epicklist#pickactions">Pick Actions</a></li>
+                        <li><a href="/rigi-docs/epicklist#settings">Additional Settings</a></li>
                     </ul>
                     <ul class="uk-nav uk-nav-default">
                         <li class="uk-nav-header">Currency Tracker</li>
-                        <li><a href="/currencytracker">Concept</a></li>
-                        <li><a href="/currencytracker#trackuntrack">Track/Untrack</a></li>
-                        <li><a href="/currencytracker#faq">FAQ</a></li>
+                        <li><a href="/rigi-docs/currencytracker">Concept</a></li>
+                        <li><a href="/rigi-docs/currencytracker#trackuntrack">Track/Untrack</a></li>
+                        <li><a href="/rigi-docs/currencytracker#faq">FAQ</a></li>
                     </ul>
                 </div>
             </div>
@@ -95,35 +95,35 @@
                 <h3>Documentation</h3>
                 <ul class="uk-nav uk-nav-default">
                     <li class="uk-nav-header">StockWise</li>
-                    <li><a href="/stockwise">Concept</a></li>
-                    <li><a href="/stockwise#searchingandscanning">Searching and Scanning</a></li>
-                    <li><a href="/stockwise#locations">Locations</a></li>
-                    <li><a href="/stockwise#stockenquiry">Stock Enquiry</a></li>
-                    <li><a href="/stockwise#suppliers">Suppliers</a></li>
-                    <li><a href="/stockwise#stockitemhistory">Stock Item History</a></li>
-                    <li><a href="/stockwise#stockin">Stock In</a></li>
-                    <li><a href="/stockwise#stockcount">Stock Count</a></li>
-                    <li><a href="/stockwise#purchaseorders">Purchase Orders</a></li>
-                    <li><a href="/stockwise#permissions">Admin Mode and Permissions</a></li>
-                    <li><a href="/stockwise#settings">Settings</a></li>
+                    <li><a href="/rigi-docs/stockwise">Concept</a></li>
+                    <li><a href="/rigi-docs/stockwise#searchingandscanning">Searching and Scanning</a></li>
+                    <li><a href="/rigi-docs/stockwise#locations">Locations</a></li>
+                    <li><a href="/rigi-docs/stockwise#stockenquiry">Stock Enquiry</a></li>
+                    <li><a href="/rigi-docs/stockwise#suppliers">Suppliers</a></li>
+                    <li><a href="/rigi-docs/stockwise#stockitemhistory">Stock Item History</a></li>
+                    <li><a href="/rigi-docs/stockwise#stockin">Stock In</a></li>
+                    <li><a href="/rigi-docs/stockwise#stockcount">Stock Count</a></li>
+                    <li><a href="/rigi-docs/stockwise#purchaseorders">Purchase Orders</a></li>
+                    <li><a href="/rigi-docs/stockwise#permissions">Admin Mode and Permissions</a></li>
+                    <li><a href="/rigi-docs/stockwise#settings">Settings</a></li>
                 </ul>
                 <ul class="uk-nav uk-nav-default">
                     <li class="uk-nav-header">ePickList</li>
-                    <li><a href="/epicklist">Concept</a></li>
-                    <li><a href="/epicklist#singleorderpick">Single Order Pick</a></li>
-                    <li><a href="/epicklist#bulkorderpick">Bulk Order Pick</a></li>
-                    <li><a href="/epicklist#pickamount">Pick Amount</a></li>
-                    <li><a href="/epicklist#picksource">Pick Source</a></li>
-                    <li><a href="/epicklist#pickoptions">Pick Options</a></li>
-                    <li><a href="/epicklist#pickrequirements">Pick Requirements</a></li>
-                    <li><a href="/epicklist#pickactions">Pick Actions</a></li>
-                    <li><a href="/epicklist#settings">Additional Settings</a></li>
+                    <li><a href="/rigi-docs/epicklist">Concept</a></li>
+                    <li><a href="/rigi-docs/epicklist#singleorderpick">Single Order Pick</a></li>
+                    <li><a href="/rigi-docs/epicklist#bulkorderpick">Bulk Order Pick</a></li>
+                    <li><a href="/rigi-docs/epicklist#pickamount">Pick Amount</a></li>
+                    <li><a href="/rigi-docs/epicklist#picksource">Pick Source</a></li>
+                    <li><a href="/rigi-docs/epicklist#pickoptions">Pick Options</a></li>
+                    <li><a href="/rigi-docs/epicklist#pickrequirements">Pick Requirements</a></li>
+                    <li><a href="/rigi-docs/epicklist#pickactions">Pick Actions</a></li>
+                    <li><a href="/rigi-docs/epicklist#settings">Additional Settings</a></li>
                 </ul>
                 <ul class="uk-nav uk-nav-default">
                     <li class="uk-nav-header">Currency Tracker</li>
-                    <li><a href="/currencytracker">Concept</a></li>
-                    <li><a href="/currencytracker#trackuntrack">Track/Untrack</a></li>
-                    <li><a href="/currencytracker#faq">FAQ</a></li>
+                    <li><a href="/rigi-docs/currencytracker">Concept</a></li>
+                    <li><a href="/rigi-docs/currencytracker#trackuntrack">Track/Untrack</a></li>
+                    <li><a href="/rigi-docs/currencytracker#faq">FAQ</a></li>
                 </ul>
             </div>
         </div>
